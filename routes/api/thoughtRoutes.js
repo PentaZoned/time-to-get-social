2b2e85d4292390be7372337d5488309a1b811ede
Bradley Le <brabrele@gmail.com>
2022-02-25 12:43:51 -0800

    added routes for users and thoughts

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,3 +1,17 @@
 const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.use('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router.use('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
 
 module.exports = router;
